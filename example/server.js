b3db2da8557e7eb00eee5913180aa10314ea7066
Evan Hahn <me@evanhahn.com>
2013-12-12 19:00:53 -0500

    clean up example server

--- example/server.js
+++ example/server.js
@@ -1,7 +1,37 @@
 var gopher = require('../gopher');
 
 var app = gopher.createServer(function(request, response) {
-	response.end('Welcome to ' + request.url);
+
+	if (request.url == '') {
+
+		response.write('iWelcome to my Node-powered Gopher server.');
+		response.write('\r\n');
+
+		response.write([
+			'0Why is Gopher Still Relevant?',
+			'/gopher/relevance.txt',
+			'gopher.floodgap.com',
+			70
+		].join('\t'));
+		response.write('\r\n');
+
+		response.write([
+			'0Check out this other URL',
+			'/something_else',
+			'localhost',
+			7070
+		].join('\t'));
+		response.write('\r\n');
+
+		response.end();
+
+	} else {
+
+		response.end('Welcome to ' + request.url);
+
+	}
+
+
 });
 
 app.listen(7070, function() {
